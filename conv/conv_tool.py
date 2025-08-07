c2c68069718671ba3536249bde2cba54da07424d
shadow_mist <shadow_mist0@proton.me>
2025-08-07 22:43:26 +0600

    Update conv_tool.py

--- conv/conv_tool.py
+++ conv/conv_tool.py
@@ -584,7 +584,7 @@ async def temperature(update:Update, content:ContextTypes.DEFAULT_TYPE):
         ]
         markup = InlineKeyboardMarkup(keyboard)
         query = update.callback_query
-        msg = await query.edit_message_text(f"Temperature represents the creativity of the bots response.\nCurrent Temperature is {settings[4]}\n\nEnter a value between 0.0 to 2.0:", reply_markup=markup)
+        msg = await query.edit_message_text(f"Temperature represents the creativity of the bots response.\nCurrent Temperature is {settings[4]}\n\nEnter a value between 0.0 to 1.0:", reply_markup=markup)
         content.user_data["t_message_id"] = msg.message_id
         return "TT"
     except Exception as e:
