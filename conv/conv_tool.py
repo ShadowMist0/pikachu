6b8d5ec7be475aa246b8773af735b2a8ee5ab9dc
shadow_mist <shadow_mist0@proton.me>
2025-08-07 22:42:27 +0600

    Update conv_tool.py

--- conv/conv_tool.py
+++ conv/conv_tool.py
@@ -584,7 +584,7 @@ async def temperature(update:Update, content:ContextTypes.DEFAULT_TYPE):
         ]
         markup = InlineKeyboardMarkup(keyboard)
         query = update.callback_query
-        msg = await query.edit_message_text(f"Temperature represents the creativity of the bots response.\nCurrent Temperature is {settings[4]}\n\nEnter a value between 0.0 to 1.0:", reply_markup=markup)
+        msg = await query.edit_message_text(f"Temperature represents the creativity of the bots response.\nCurrent Temperature is {settings[4]}\n\nEnter a value between 0.0 to 2.0:", reply_markup=markup)
         content.user_data["t_message_id"] = msg.message_id
         return "TT"
     except Exception as e:
@@ -605,7 +605,7 @@ async def take_temperature(update:Update, content:ContextTypes.DEFAULT_TYPE):
             await content.bot.delete_message(chat_id=user_id, message_id=content.user_data.get("t_message_id"))
             await update.message.delete()
             return ConversationHandler.END
-        if data > 1.0 or data < 0.0:
+        if data > 2.0 or data < 0.0:
             await update.message.reply_text("Invalid Input. Temperature should be between 0.0 to 1.0")
             await content.bot.delete_message(chat_id=user_id, message_id=content.user_data.get("t_message_id"))
             await update.message.delete()
